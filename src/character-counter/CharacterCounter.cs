4c4fb969ec9d85c183ac14d3a65b66dd2cdc0de6
yang vieira <yangflix3@gmail.com>
2022-08-07 11:36:28 -0400

    character counter feito

--- src/character-counter/CharacterCounter.cs
+++ src/character-counter/CharacterCounter.cs
@@ -1,17 +1,35 @@
 ﻿using System.Diagnostics;
 
 namespace CharacterCounter;
-public class CharacterCounter
-{
+public static class CharacterCounter
+{ 
+
+     public static void Main() {
+        Action("oi tudo bem");
+     }
+    
         /// <summary>
         /// This function aims to perform the character count of a given text
         /// </summary>
         /// <param name="text"> A value of type string, the text to be calculated</param>
         /// <returns>The number of characters of the text passed</returns>
         /// <exception cref="NullReferenceException">If text is null throw exception </exception>
-        public int Action(string text)
+        
+        public static int Action(string text)
         {
-            throw new NotImplementedException();    
+          try {
+            if(text == null || text.Length == 0) {
+            throw new NullReferenceException("O valor não pode ser nulo");
+            } 
+            
+            return text.Length;
+
+          } catch(NullReferenceException ex) {
+           Console.WriteLine(ex.Message);
+           throw;
+          }
+
+    
+         }
         } 
 
-}
